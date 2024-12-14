3ce88e054756ed0140a7fc9da333b972b4e6a389
Daphish <ivan.davila.2003@gmail.com>
2024-12-13 21:39:08 -0600

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-white dark:bg-gray-900">
+        <h1 className="text-black dark:text-white">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
